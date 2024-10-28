ebdcc8d99d29df7ec246882f5d7167db01b1b80d
JKaraky <jawad.alkaraky@gmail.com>
2024-10-28 10:44:05 +0200

    AddEdit product page

--- TequilasRestaurant/Controllers/ProductController.cs
+++ TequilasRestaurant/Controllers/ProductController.cs
@@ -7,15 +7,37 @@ namespace TequilasRestaurant.Controllers
     public class ProductController : Controller
     {
         private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
 
         public ProductController(ApplicationDbContext context)
         {
             products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await products.GetAllAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                ViewBag.Operation = "Edit";
+                return View();
+            }
         }
     }
 }
